e56deb837c1206a80190fd40f09bb1f2898dae08
Shuhei <shuhei.112028@gmail.com>
2018-06-06 12:39:19 +0900

    add the create-work-page

---
+++ src/components/CreateWorkPage.tsx
@@ -0,0 +1,78 @@
+import React from "react";
+import styled from "styled-components";
+import {
+    Button,
+    Card,
+    CardActions,
+    CardMedia,
+    CardContent,
+    Typography,
+    TextField,
+    InputAdornment
+} from "@material-ui/core";
+
+export default class extends React.Component<{onMenuButtonClick: (event: React.MouseEvent<HTMLElement>) => void}> {
+    state = {
+        userMenuAnchorEl: undefined,
+        userMenuOpend: false
+    };
+
+    render() {
+        return (
+            <Host>
+                <StyledCard>
+                    <StyledCardMedia
+                        image={`http://placehold.jp/24/8BC34A/fff/600x400.png`}
+                        title="Contemplative Reptile"
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="headline" component="h2">
+                            Lizard
+                        </Typography>
+                    </CardContent>
+                    <CardActions>
+                        <Button size="small" color="primary">
+                        Share
+                        </Button>
+                        <Button size="small" color="primary">
+                        Learn More
+                        </Button>
+                    </CardActions>
+                </StyledCard>
+                <StyleTextField
+                    id="title"
+                    label="Title"
+                    margin="normal"
+                />
+            </Host>
+        );
+    }
+}
+
+const Host = styled.div`
+    margin: 3rem;
+    display: flex;
+    flex-wrap: wrap;
+`;
+
+const StyledCard = styled(Card)`
+    && {
+        margin: 1rem;
+        min-width: 20rem;
+        max-width: 30rem;
+    }
+`;
+
+const StyledCardMedia = styled(CardMedia)`
+    && {
+        height: 0;
+        padding-top: 56.25%;
+    }
+`;
+const StyleTextField = styled(TextField)`
+    && {
+      marginLeft: theme.spacing.unit;
+      marginRight: theme.spacing.unit;
+      width: 200;
+    }
+`;
