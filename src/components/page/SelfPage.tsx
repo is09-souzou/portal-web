d8f82aca85845de197554d09dbca3ca0a232a6b4
alf852 <hsa00852@gmail.com>
2018-06-16 18:56:56 +0900

    update SelfPage

--- src/components/page/SelfPage.tsx
+++ src/components/page/SelfPage.tsx
@@ -2,33 +2,48 @@ import React from "react";
 import { Query } from "react-apollo";
 import styled from "styled-components";
 import QueryGetUser from "../../GraphQL/query/QueryGetUser";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import {
     Button,
-    Card,
-    CardActions,
-    CardContent,
-    CardMedia,
+    Divider,
+    ExpansionPanel,
+    ExpansionPanelDetails,
+    ExpansionPanelSummary,
+    ExpansionPanelActions,
     TextField,
     Typography
-} from "@material-ui/core"
-// TODO ↑ セミコロン忘れ エディタにtslintの拡張いれてない？
+} from "@material-ui/core";
 import { PageComponentProps } from "../../App";
 
 interface State {
     userEditing: boolean;
+    emailEditing: boolean;
+    careerEditing: boolean;
+    messageEditing: boolean;
 }
 
 export default class extends React.Component<PageComponentProps<{}>, State> {
 
     componentWillMount() {
         this.setState({
-            userEditing: false
+            nameEditing: false,
+            emailEditing: false,
+            careerEditing: false,
+            messageEditing: false
         });
     }
 
-    userEditingStart = () => this.setState({ userEditing: true });
+    nameEditingStart = () => this.setState({ nameEditing: true });
+    nameEditingEnd = () => this.setState({ nameEditing: false });
 
-    userEditingEnd = () => this.setState({ userEditing: false });
+    emailEditingStart = () => this.setState({ emailEditing: true });
+    emailEditingEnd = () => this.setState({ emailEditing: false });
+
+    careerEditingStart = () => this.setState({ careerEditing: true });
+    careerEditingEnd = () => this.setState({ careerEditing: false });
+
+    messageEditingStart = () => this.setState({ messageEditing: true });
+    messageEditingEnd = () => this.setState({ messageEditing: false });
 
     render() {
         const {
@@ -56,57 +71,130 @@ export default class extends React.Component<PageComponentProps<{}>, State> {
 
                     return (
                         <div>
-                            {/* TODO  <StyledCard> は条件分岐によって変化ないので共通化していいかと　*/}
-                            {/* TODO ↓ this.state.userEditing */}
-                            {this.userEditing ?
-                                <StyledCard>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="display1" component="h2">
-                                            Profile
-                                        </Typography>
-                                        <TextField
+                            <StyledPanel>
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography>
+                                        displayName
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <StyledPanelDetails>
+                                    {this.state.nameEditing ?
+                                        <StyledTextField
                                             id="self-name"
-                                            label="DisplayName"
-                                            value={data.getUser.name}
+                                            default={data.getUser.name}
                                             margin="normal"
                                         />
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small" color="primary" onClick={this.userEditingEnd}>
-                                            End
+                                    :
+                                        <StyledPersonalData>
+                                            {data.getUser.name}
+                                        </StyledPersonalData>
+                                    }
+                                </StyledPanelDetails>
+                                <StyledPanelActions>
+                                    {this.state.nameEditing ?
+                                        <Button color="primary" onClick={this.nameEditingEnd}>
+                                            Save
                                         </Button>
-                                    </CardActions>
-                                </StyledCard>
-                            :
-                                <StyledCard>
-                                    <CardContent>
-                                        <Typography gutterBottom variant="display1" component="h2">
-                                            Profile
-                                        </Typography>
-                                        <StyledCardMedia
-                                            image={data.getUser.avatorURI}
-                                            title="avator"
-                                        />
-                                        <Typography gutterBottom variant="body1" component="label">
-                                            DisplayName : {data.getUser.name}
-                                        </Typography>
-                                        <Typography gutterBottom variant="body1" component="label">
-                                            Email : {data.getUser.email}
-                                        </Typography>
-                                        <Typography gutterBottom variant="body1" component="label">
-                                            Carrer : {data.getUser.career}
-                                        </Typography>
-                                        <Typography gutterBottom variant="body1" component="label">
-                                            Message : {data.getUser.messeage}
-                                        </Typography>
-                                    </CardContent>
-                                    <CardActions>
-                                        <Button size="small" color="primary" onClick={this.userEditingStart}>
+                                    :
+                                        <Button color="primary" onClick={this.nameEditingStart}>
                                             Edit
                                         </Button>
-                                    </CardActions>
-                                </StyledCard>
-                            }
+                                    }
+                                </StyledPanelActions>
+                            </StyledPanel>
+                            <StyledPanel>
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography>
+                                        Email
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <StyledPanelDetails>
+                                    {this.state.emailEditing ?
+                                        <StyledTextField
+                                            id="self-email"
+                                            default={data.getUser.email}
+                                            margin="normal"
+                                        />
+                                    :
+                                        <StyledPersonalData>
+                                            {data.getUser.email}
+                                        </StyledPersonalData>
+                                    }
+                                </StyledPanelDetails>
+                                <StyledPanelActions>
+                                    {this.state.emailEditing ?
+                                        <Button color="primary" onClick={this.emailEditingEnd}>
+                                            Save
+                                        </Button>
+                                    :
+                                        <Button color="primary" onClick={this.emailEditingStart}>
+                                            Edit
+                                        </Button>
+                                    }
+                                </StyledPanelActions>
+                            </StyledPanel>
+                            <StyledPanel>
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography>
+                                        Career
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <StyledPanelDetails>
+                                    {this.state.careerEditing ?
+                                        <StyledTextField
+                                            id="self-name"
+                                            default={data.getUser.career}
+                                            margin="normal"
+                                        />
+                                    :
+                                        <StyledPersonalData>
+                                            {data.getUser.career}
+                                        </StyledPersonalData>
+                                    }
+                                </StyledPanelDetails>
+                                <StyledPanelActions>
+                                    {this.state.careerEditing ?
+                                        <Button color="primary" onClick={this.careerEditingEnd}>
+                                            Save
+                                        </Button>
+                                    :
+                                        <Button color="primary" onClick={this.careerEditingStart}>
+                                            Edit
+                                        </Button>
+                                    }
+                                </StyledPanelActions>
+                            </StyledPanel>
+                            <StyledPanel>
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography>
+                                        Message
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <StyledPanelDetails>
+                                    {this.state.messageEditing ?
+                                        <StyledTextField
+                                            id="self-name"
+                                            default={data.getUser.messeage}
+                                            margin="normal"
+                                        />
+                                    :
+                                        <StyledPersonalData>
+                                            {data.getUser.messeage}
+                                        </StyledPersonalData>
+                                    }
+                                </StyledPanelDetails>
+                                <StyledPanelActions>
+                                    {this.state.messageEditing ?
+                                        <Button color="primary" onClick={this.messageEditingEnd}>
+                                            Save
+                                        </Button>
+                                    :
+                                        <Button color="primary" onClick={this.messageEditingStart}>
+                                            Edit
+                                        </Button>
+                                    }
+                                </StyledPanelActions>
+                            </StyledPanel>
                         </div>
                     );
                 }}
@@ -115,16 +203,33 @@ export default class extends React.Component<PageComponentProps<{}>, State> {
     }
 }
 
-const StyledCard = styled(Card)`
+const StyledPersonalData = styled(Typography)`
     && {
-        margin: 1rem;
-        min-width: 20rem;
-        max-width: 100%;
+        margin-left: 2rem;
     }
 `;
-const StyledCardMedia = styled(CardMedia)`
+const StyledPanel = styled(ExpansionPanel)`
     && {
-        height: 0;
-        padding-top: 10%;
+        margin-left: 3rem;
+        min-width: 20rem;
+        max-width: 90%;
+    }
+`;
+const StyledPanelDetails = styled(ExpansionPanelDetails)`
+    && {
+        display: flex;
+        flex-direction: column
+    }
+`;
+const StyledPanelActions = styled(ExpansionPanelActions)`
+    && {
+        height: 1rem;
+        padding-top: 2%;
+    }
+`;
+const StyledTextField = styled(TextField)`
+    && {
+        margin-left: 2rem;
+        width: 20rem;
     }
 `;
