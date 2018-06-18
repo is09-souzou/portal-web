b2fa4ceec3a1db5ca7c4f3320e05823b32f9a2c5
alf852 <hsa00852@gmail.com>
2018-06-18 15:13:19 +0900

    SelfPage fix

--- src/components/page/SelfPage.tsx
+++ src/components/page/SelfPage.tsx
@@ -10,27 +10,35 @@ import {
     ExpansionPanelDetails,
     ExpansionPanelSummary,
     ExpansionPanelActions,
+    TextField,
     Typography
 } from "@material-ui/core";
 import { PageComponentProps } from "../../App";
 
-type Item = "displayName" | "email";
+type Item = "displayName" | "email" | "career" | "message";
 
 interface State {
     opendItem?: Item;
+    userEditing: boolean;
 }
 
 export default class extends React.Component<PageComponentProps<{}>, State> {
 
     componentWillMount() {
         this.setState({
-            opendItem: undefined
+            opendItem: undefined,
+            userEditing: false
         });
     }
 
     handleChange = (item: Item) => () => this.setState({
-        opendItem: this.state.opendItem === item ? undefined : item
+        opendItem: this.state.opendItem === item ? undefined : item,
+        userEditing: false
     })
+
+    userEditingStart = () => this.setState({ userEditing: true });
+
+    userEditingEnd = () => this.setState({ userEditing: false });
 
     render() {
         const {
@@ -58,29 +66,56 @@ export default class extends React.Component<PageComponentProps<{}>, State> {
 
                     return (
                         <div>
+                            {this.state.userEditing ?
+                                <StyledPanel
+                                    expanded={this.state.opendItem === "displayName"}
+                                    onChange={this.handleChange("displayName")}
+                                >
+                                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                        <Typography>
+                                            DisplayName
+                                        </Typography>
+                                    </ExpansionPanelSummary>
+                                    <StyledPanelDetails>
+                                        <StyledTextField
+                                            id="self-name"
+                                            defaultValue={data.getUser.name}
+                                            margin="normal"
+                                            required
+                                        />
+                                    </StyledPanelDetails>
+                                    <Divider />
+                                    <StyledPanelActions>
+                                        <Button type="submit" color="primary" onClick={this.userEditingEnd}>
+                                            save
+                                        </Button>
+                                    </StyledPanelActions>
+                                </StyledPanel>
+                            :
+                                <StyledPanel
+                                    expanded={this.state.opendItem === "displayName"}
+                                    onChange={this.handleChange("displayName")}
+                                >
+                                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                        <Typography>
+                                            DisplayName
+                                        </Typography>
+                                    </ExpansionPanelSummary>
+                                    <StyledPanelDetails>
+                                        <StyledPersonalData>
+                                            {data.getUser.name}
+                                        </StyledPersonalData>
+                                    </StyledPanelDetails>
+                                    <Divider />
+                                    <StyledPanelActions>
+                                        <Button color="primary" onClick={this.userEditingStart}>
+                                            edit
+                                        </Button>
+                                    </StyledPanelActions>
+                                </StyledPanel>
+                            }
                             <StyledPanel
-                                expanded={this.state.opendItem === "displayName"}
-                                onChange={this.handleChange("displayName")}
-                            >
-                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
-                                    <Typography>
-                                        DisplayName
-                                    </Typography>
-                                </ExpansionPanelSummary>
-                                <StyledPanelDetails>
-                                    <StyledPersonalData>
-                                        {data.getUser.name}
-                                    </StyledPersonalData>
-                                </StyledPanelDetails>
-                                <Divider />
-                                <StyledPanelActions>
-                                    <Button color="primary">
-                                        edit
-                                    </Button>
-                                </StyledPanelActions>
-                            </StyledPanel>
-                            <StyledPanel
-                                expanded={this.state.opendItem === `email`}
+                                expanded={this.state.opendItem === "email"}
                                 onChange={this.handleChange("email")}
                             >
                                 <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
@@ -91,6 +126,48 @@ export default class extends React.Component<PageComponentProps<{}>, State> {
                                 <StyledPanelDetails>
                                     <StyledPersonalData>
                                         {data.getUser.email}
+                                    </StyledPersonalData>
+                                </StyledPanelDetails>
+                                <Divider />
+                                <StyledPanelActions>
+                                    <Button color="primary">
+                                        edit
+                                    </Button>
+                                </StyledPanelActions>
+                            </StyledPanel>
+                            <StyledPanel
+                                expanded={this.state.opendItem === "career"}
+                                onChange={this.handleChange("career")}
+                            >
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography>
+                                        Career
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <StyledPanelDetails>
+                                    <StyledPersonalData>
+                                        {data.getUser.career}
+                                    </StyledPersonalData>
+                                </StyledPanelDetails>
+                                <Divider />
+                                <StyledPanelActions>
+                                    <Button color="primary">
+                                        edit
+                                    </Button>
+                                </StyledPanelActions>
+                            </StyledPanel>
+                            <StyledPanel
+                                expanded={this.state.opendItem === "message"}
+                                onChange={this.handleChange("message")}
+                            >
+                                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                                    <Typography>
+                                        Message
+                                    </Typography>
+                                </ExpansionPanelSummary>
+                                <StyledPanelDetails>
+                                    <StyledPersonalData>
+                                        {data.getUser.message}
                                     </StyledPersonalData>
                                 </StyledPanelDetails>
                                 <Divider />
@@ -130,5 +207,12 @@ const StyledPanelActions = styled(ExpansionPanelActions)`
     && {
         height: 1rem;
         padding-top: 2%;
+    }
+`;
+
+const StyledTextField = styled(TextField)`
+    && {
+        margin-left: 2rem;
+        width: 20rem;
     }
 `;
