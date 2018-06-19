2846b1a73a91c090da78d87751625f48611af883
杉山希望 <rioc.sugiyama@gmail.com>
2018-06-19 14:11:21 +0900

    update mutation

--- src/GraphQL/mutation/MutationCreateWork.ts
+++ src/GraphQL/mutation/MutationCreateWork.ts
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 
 export default gql(`
     mutation createWork(
-        $work: WorkCreate
+        $work: WorkCreate!
     ) {
         createWork(
             work: $work
