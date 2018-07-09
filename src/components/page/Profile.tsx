0b308072fe091b917b7459363366e7a1f98411ab
ren hashimoto <hashiserver@gmail.com>
2018-07-09 12:00:51 +0900

    Add Update Password

--- src/components/page/Profile.tsx
+++ src/components/page/Profile.tsx
@@ -405,6 +405,44 @@ export default class extends React.Component<PageComponentProps<{}>, State> {
                             />
                         </form>
                     </div>
+                    <div>
+                    <form
+                        // tslint:disable-next-line:jsx-no-lambda
+                        onSubmit={async e => {
+                            e.preventDefault();
+
+                            const oldPassword = (e.target as any).elements["update-oldPassword"].value;
+                            const newPassword = (e.target as any).elements["update-newPassword"].value;
+                            try {
+                                await this.props.auth.updatePassword(oldPassword, newPassword);
+                            } catch (e) {
+                                notificationListener.errorNotification(e);
+                            }
+                        }}
+                    >
+                        <div>
+                            <TextField
+                                id="update-oldPassword"
+                                label="oldPassword"
+                                margin="normal"
+                                type="password"
+                            />
+                            <TextField
+                                id="update-newPassword"
+                                label="newPassword"
+                                margin="normal"
+                                type="password"
+                            />
+                        <Button
+                            component="button"
+                            color="primary"
+                            type="submit"
+                        >
+                            update
+                        </Button>
+                        </div>
+                    </form>
+                </div>
                 </StyledSetting>
             </Page>
         );
