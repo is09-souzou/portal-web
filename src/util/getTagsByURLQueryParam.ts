e5a4a4be007eb81bf124aa0f72b4b8093e1e0c18
杉山希望 <rioc.sugiyama@gmail.com>
2018-07-14 21:07:11 +0900

    fix(getTagsByURLQueryParam.ts): add if tags not in queryParam func

--- src/util/getTagsByURLQueryParam.ts
+++ src/util/getTagsByURLQueryParam.ts
@@ -6,5 +6,6 @@ export default (history: H.History) => {
     const tags = !queryParam        ? []
                : queryParam["tags"] ? queryParam["tags"].split(",")
                :                      [];
-    return queryParam && !(tags.length === 1 && tags[0] === "") ? queryParam["tags"].split(",") : [];
+    return queryParam && !((tags.length === 1 && tags[0] === "") || tags.length === 0) ? queryParam["tags"].split(",")
+                       :                                                                 [];
 };
