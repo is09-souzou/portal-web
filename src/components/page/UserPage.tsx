ace411ee0986a012267fa8f2adbd108f38f567dd
ren hashimoto <hashiserver@gmail.com>
2018-07-24 01:58:51 +0900

    selfpage create design

--- src/components/page/UserPage.tsx
+++ src/components/page/UserPage.tsx
@@ -1,5 +1,9 @@
 import React, { Fragment } from "react";
+import {
+    Avatar
+} from "@material-ui/core";
 import gql                 from "graphql-tag";
+import styled              from "styled-components";
 import { Query }           from "react-apollo";
 import { PageComponentProps } from "../../App";
 import ErrorPage              from "../ErrorPage";
@@ -61,14 +65,48 @@ export default class UserListPage extends React.Component<PageComponentProps<{id
                         const user = data.getUser as User;
 
                         return (
-                            <div>
-                                <div>{user.id}</div>
-                                <div>{user.avatarUri}</div>
-                                <div>{user.career}</div>
-                                <div>{user.displayName}</div>
-                                <div>{user.email}</div>
-                                <div>{user.message}</div>
-                            </div>
+                            <Host>
+                                <UserHost>
+                                    <UserHedaerImage
+                                        src={user.avatarUri}
+                                    />
+                                    <div>
+                                        <div>
+                                            {user.displayName}
+                                        </div>
+                                        <div>
+                                            {user.email}
+                                        </div>
+                                    </div>
+                                    <UserAvatar
+                                        src={user.avatarUri}
+                                    />
+                                </UserHost>
+                                <UserContent>
+                                    <div>
+                                        <div>
+                                            Follow Follower
+                                        </div>
+                                        <div>
+                                            {user.message}
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            description
+                                            事柄を説明し、正確に伝達することを目的とする文章。叙情文・叙事文・叙景文などに対する語。
+                                            1
+                                            2
+                                            3
+                                            4
+                                            5
+                                        </div>
+                                        <div>
+                                            {user.career}
+                                        </div>
+                                    </div>
+                                </UserContent>
+                            </Host>
                         );
                     }}
                 </Query>
@@ -76,3 +114,59 @@ export default class UserListPage extends React.Component<PageComponentProps<{id
         );
     }
 }
+
+const Host = styled.div`
+    display: flex;
+    flex-direction: column;
+    margin-top: -7rem;
+`;
+
+const UserHost = styled.div`
+    position: relative;
+    > :nth-child(2) {
+        background-color: rgba( 220, 220, 220, 0.55 );
+        margin-left: 16rem;
+        bottom: 1rem;
+        position: absolute;
+        z-index: 1;
+    }
+`;
+
+const UserHedaerImage = styled.img`
+    position: relative;
+    height: 19rem;
+    width: 100%;
+    object-fit: cover;
+`;
+
+const UserAvatar = styled(Avatar)`
+    && {
+        position: absolute;
+        bottom: -5rem;
+        border: 1px solid #ccc;
+        width: 12rem;
+        height: 12rem;
+        margin: 0 3rem;
+    }
+`;
+
+const UserContent = styled.div`
+    display: flex;
+    margin-top: 6rem;
+    > :nth-child(1){
+        width: 16rem;
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        :nth-child(n){
+            display: inline-grid;
+        }
+    }
+    > :nth-child(2){
+        flex-grow: 1;
+        min-height: 100%;
+        :nth-child(n){
+            display: inline-grid;
+        }
+    }
+`;
