98f088d134a214ee45117e8f82054a04f9d2b537
ren hashimoto <hashiserver@gmail.com>
2018-12-21 09:57:17 +0900

    [Modify]fix to display user

--- src/components/page/UserPage.tsx
+++ src/components/page/UserPage.tsx
@@ -294,7 +294,12 @@ export default class UserListPage extends React.Component<PageComponentProps<{id
                                         </Tabs>
                                     </Footer>
                                     <Fab
-                                        style={{ visibility: user.id === auth.token!.payload.sub && contentType === "user" ? "visible" : "hidden" }}
+                                        // tslint:disable-next-line:max-line-length
+                                        style={{
+                                            visibility: (
+                                                    (auth.token && user.id === auth.token!.payload.sub) && contentType === "user" ? "visible" : "hidden"
+                                                )
+                                        }}
                                         // tslint:disable-next-line:jsx-no-lambda
                                         onClick={() => history.push("/profile")}
                                     >
