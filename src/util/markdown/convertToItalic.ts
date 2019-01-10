5623ec74a70449eaa5f9984a351bedff9f7bae51
hashiserver <hashiserver@gmail.com>
2019-01-10 09:51:01 +0900

    [FIX]convertToItalic

--- src/util/markdown/convertToItalic.ts
+++ src/util/markdown/convertToItalic.ts
@@ -4,5 +4,5 @@ export default (value: string, selectionNumbers: [number, number]): [string, [nu
     baseTextConverter({
         value,
         selectionNumbers,
-        syntax: "_"
+        syntax: "*"
     });
