5df047b9225ffd355ef084d2e75cc40bed770f45
NozomiSugiyama <rioc.sugiyama@gmail.com>
2019-01-11 11:12:59 +0900

    fix(insertSeparator): insert separator when inputed only one row

--- src/util/markdown/insertSeparator.ts
+++ src/util/markdown/insertSeparator.ts
@@ -3,12 +3,10 @@ import getLines from "src/util/markdown/getLines";
 export default (value: string, selectionNumbers: [number, number]): [string, [number, number]] => {
     const lines = getLines(value, selectionNumbers);
 
-    let adjustmentCount = 0;
     const convertedValue = value.split("\n")
         .map((x: string, i: number) => {
-            if (i + 1 - 1 === lines[1]) {
-                adjustmentCount = adjustmentCount + 3;
-                return x.replace(/^/g, "---\n");
+            if (i + 1 === lines[1]) {
+                return x.replace(/$/g, "\n---");
             }
             return x;
         })
@@ -16,9 +14,6 @@ export default (value: string, selectionNumbers: [number, number]): [string, [nu
 
     return [
         convertedValue,
-        [
-            selectionNumbers[0] + adjustmentCount,
-            selectionNumbers[1] + adjustmentCount
-        ]
+        selectionNumbers
     ];
 };
