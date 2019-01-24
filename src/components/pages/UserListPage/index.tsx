6de61cc43a30f827e2b4868230a55d0e037152e6
NozomiSugiyama <rioc.sugiyama@gmail.com>
2019-01-24 16:19:37 +0900

    feat(UserListPage): Add operation handler of Not found

--- src/components/pages/UserListPage/index.tsx
+++ src/components/pages/UserListPage/index.tsx
@@ -147,6 +147,8 @@ class UserListPageWrapper extends React.Component<UserListPageWrapperProps, Stat
             );
         }
 
+        console.log(this.state);
+
         return (
             <Host
                 ref={props.ref as any}
@@ -156,9 +158,13 @@ class UserListPageWrapper extends React.Component<UserListPageWrapperProps, Stat
                     title={<LocationText text="User list"/>}
                 />
                 {
-                    this.state.loading                                              ? <GraphQLProgress/>
-                  : !(this.state.userConnection && this.state.userConnection.items) ? <NotFound/>
-                  :                                                                   (
+                    this.state.loading                         ? <GraphQLProgress/>
+                  : !(
+                        this.state.userConnection
+                     && this.state.userConnection.items
+                     && this.state.userConnection.items.length === 0
+                     )                                         ? <NotFound/>
+                  :                                              (
                         <UserListPage
                             userConnection={this.state.userConnection}
                             fetchMore={() => undefined}
