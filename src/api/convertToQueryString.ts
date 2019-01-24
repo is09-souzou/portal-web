443798b6aa7a528c03d8610f4a227a54f9f9e3b5
NozomiSugiyama <rioc.sugiyama@gmail.com>
2019-01-24 13:36:50 +0900

    feat(api): Create api of es

--- src/api/convertToQueryString.ts
+++ src/api/convertToQueryString.ts
@@ -1,11 +1,11 @@
 import deduplicationFromArray from "src/util/deduplicationFromArray";
 
-export default (param: string, tags: string[], currentTags?: string[]) =>
+export default (param: string, list: string[], currentList?: string[]) =>
     // tslint:disable-next-line:prefer-template
     `?${param}=` + (
-        currentTags ? (
-            deduplicationFromArray(currentTags.concat(tags))
+        currentList ? (
+            deduplicationFromArray(currentList.concat(list))
                 .reduce((prev, next, i) => i === 0 ? next : `${prev},${next}`, "")
         )
-      :               tags.reduce((prev, next, i) => i === 0 ? next : `${prev},${next}`, "")
+      :               list.reduce((prev, next, i) => i === 0 ? next : `${prev},${next}`, "")
     );
