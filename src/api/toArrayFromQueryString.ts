538dbcc49844273fdebb43b28471dba4806a911b
hashiserver <hashiserver@gmail.com>
2019-01-30 19:50:09 +0900

    fix(api): Add deduplication logic

--- src/api/toArrayFromQueryString.ts
+++ src/api/toArrayFromQueryString.ts
@@ -1,11 +1,14 @@
 import * as H from "history";
 import toObjectFromURIQuery from "src/api/toObjectFromURIQuery";
+import deduplicationFromArray from "src/util/deduplicationFromArray";
 
 export default (param: string, history: H.History): string[] => {
     const queryParam = toObjectFromURIQuery(history.location.search);
-    const tags = !queryParam        ? []
-               : queryParam[param]  ? queryParam[param].split(",")
-               :                      [];
+    const tags = deduplicationFromArray(
+        !queryParam        ? []
+      : queryParam[param]  ? queryParam[param].split(",")
+      :                      []
+    );
     return queryParam && !((tags.length === 1 && tags[0] === "") || tags.length === 0) ? queryParam[param].split(",").map(x => (decodeURIComponent(x)))
                        :                                                                 [];
 };
