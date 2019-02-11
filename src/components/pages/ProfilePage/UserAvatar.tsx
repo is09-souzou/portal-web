7a8f2d1f3c1a6eaaeb6d84dacfe46350d6218ba7
alf852 <hsa00852@gmail.com>
2019-02-12 05:44:31 +0900

    feat(ProfilePage): add avatar image update

--- src/components/pages/ProfilePage/UserAvatar.tsx
+++ src/components/pages/ProfilePage/UserAvatar.tsx
@@ -7,6 +7,9 @@ export default styled(Avatar as React.SFC<AvatarProps>)`
         border: 1px solid #ccc;
         width: 10rem;
         height: 10rem;
+        :hover {
+            cursor : pointer;
+        }
         @media (max-width: 768px) {
             width: 6rem;
             height: 6rem;
--- src/components/pages/ProfilePage/index.tsx
+++ src/components/pages/ProfilePage/index.tsx
@@ -24,6 +24,7 @@ import ChipList from "src/components/pages/ProfilePage/ChipList";
 import Host from "src/components/pages/ProfilePage/Host";
 import ProfileContent from "src/components/pages/ProfilePage/ProfileContent";
 import ShortTextField from "src/components/pages/ProfilePage/ShortTextField";
+import UserAvatar from "src/components/pages/ProfilePage/UserAvatar";
 import ErrorTemplate from "src/components/templates/ErrorTemplate";
 import AuthContext, { AuthValue } from "src/contexts/AuthContext";
 import LocalizationContext from "src/contexts/LocalizationContext";
@@ -174,22 +175,30 @@ const ProfilePage = (
             }
         >
             <ProfileContent>
-                <ShortTextField
-                    id="profile-name"
-                    margin="dense"
-                    label={<LocationText text="Display name"/>}
-                    defaultValue={user.displayName}
-                    required
-                    inputRef={displayNameInputElement}
-                />
-                <ShortTextField
-                    id="profile-email"
-                    margin="dense"
-                    label={<LocationText text="Mail address"/>}
-                    type="email"
-                    defaultValue={user.email}
-                    inputRef={emailInputElement}
-                />
+                <div>
+                    <UserAvatar
+                        src={user.avatarUri}
+                        onClick={() => setEditableAvatarDialogOpen(true)}
+                    />
+                    <div>
+                        <ShortTextField
+                            id="profile-name"
+                            margin="dense"
+                            label={<LocationText text="Display name"/>}
+                            defaultValue={user.displayName}
+                            required
+                            inputRef={displayNameInputElement}
+                        />
+                        <ShortTextField
+                            id="profile-email"
+                            margin="dense"
+                            label={<LocationText text="Mail address"/>}
+                            type="email"
+                            defaultValue={user.email}
+                            inputRef={emailInputElement}
+                        />
+                    </div>
+                </div>
                 <TextField
                     id="profile-message"
                     label={<LocationText text="Message"/>}
@@ -290,7 +299,7 @@ const ProfilePage = (
                             color="primary"
                             type="submit"
                         >
-                            <LocationText text="Submit"/>
+                            <LocationText text="Update"/>
                         </Button>
                     </DialogActions>
                 </form>
